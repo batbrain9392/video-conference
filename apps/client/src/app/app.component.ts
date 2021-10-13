dbd81fa1bf57b36a5c4bc5475493ab2e87221c46
Debmallya Bhattacharya <debmallya.bhattacharya@blueface.com>
2021-10-13 06:21:16 +0100

    using ng-web-apis

--- apps/client/src/app/app.component.ts
+++ apps/client/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'video-conference-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('me') videoMeRef?: ElementRef<HTMLVideoElement>;
+  @ViewChild('other') videoOtherRef?: ElementRef<HTMLVideoElement>;
+
+  ngAfterViewInit(): void {
+    console.log(this.videoMeRef?.nativeElement);
+  }
 }
