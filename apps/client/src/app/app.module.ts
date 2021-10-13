1b663fd397408457496eaa39d6bdf4f27181aaf4
Debmallya Bhattacharya <debmallya.bhattacharya@blueface.com>
2021-10-13 05:24:56 +0100

    firebase setup

--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- apps/client/src/environments/environment.ts
+++ apps/client/src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'vc-batbrain9392',
+    appId: '1:505984550308:web:56fb162f9834a9428c6d31',
+    storageBucket: 'vc-batbrain9392.appspot.com',
+    apiKey: 'AIzaSyA7nT0hr3ZcZwOvxaQdWxub3AXJb3qsgeM',
+    authDomain: 'vc-batbrain9392.firebaseapp.com',
+    messagingSenderId: '505984550308',
+    measurementId: 'G-ZQ80PXEBY7',
+  },
   production: false,
 };
 
